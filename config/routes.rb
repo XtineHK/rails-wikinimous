f50c207a37571997f5da9d3ea88002859b782f07
Christine Brendle <christinebrendle@gmail.com>
2020-11-10 23:34:27 +0100

    wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+# read all articles
+get "articles", to: "articles#index"
+
+# create an article : 2 steps first to get the form [new(get)] + then to add it [create(post)]
+get "articles/new", to: "articles#new", as: "new_article"
+post "articles", to: "articles#create"
+
+# read one article
+get "articles/:id", to: "articles#show", as: "article"
+
+# update an article
+get "articles/:id/edit", to: "articles#edit", as: "edit_article"
+patch "articles/:id", to: "articles#update"
+
+# delete an article
+delete "articles/:id", to: "articles#destroy"
+
+
+
+
+
+
+
+
+
+
+
+
 end
